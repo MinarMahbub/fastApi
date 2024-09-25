0d1cbc0f274449b76a23ff116fd7317ebfac0aec
minar <mahabubruet15@gmail.com>
2024-09-25 16:49:58 +0600

    Request body

--- main.py
+++ main.py
@@ -43,11 +43,39 @@
 #     item = {"item_id" : item_id , "needy": needy}
 #     return item
 
+# from fastapi import FastAPI
+# app = FastAPI()
+
+# @app.get({"/items/item_id"})
+# async def read_user_item(
+#     item_id:str, needy: str, skip : int = 0, limit:  int | None = None):
+#     item = {"item_id" : item_id, "needy": needy, "skip": skip, "limit": limit}
+#     return item
+
+
+from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
 app = FastAPI()
 
-@app.get({"/items/item_id"})
-async def read_user_item(
-    item_id:str, needy: str, skip : int = 0, limit:  int | None = None):
-    item = {"item_id" : item_id, "needy": needy, "skip": skip, "limit": limit}
-    return item
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"Price_with_tax" : price_with_tax})
+    return item_dict
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: Union[str, None] = None):
+    result = {"Item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q" : q})
+    return result
