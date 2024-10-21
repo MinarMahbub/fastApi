b1bed54dd062e9e84b84573bea311ff1c91b9c17
minar <mahabubruet15@gmail.com>
2024-10-21 20:47:21 +0600

    Response Return type discussion

--- main.py
+++ main.py
@@ -53,29 +53,442 @@
 #     return item
 
 
-from typing import Union
-from fastapi import FastAPI
-from pydantic import BaseModel
+# from typing import Union
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+
+# class Item(BaseModel):
+#     name: str
+#     description: Union[str, None] = None
+#     price: float
+#     tax: Union[float, None] = None
+
+# app = FastAPI()
+
+# @app.post("/items/")
+# async def create_item(item: Item):
+#     item_dict = item.dict()
+#     if item.tax:
+#         price_with_tax = item.price + item.tax
+#         item_dict.update({"Price_with_tax" : price_with_tax})
+#     return item_dict
+
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item, q: Union[str, None] = None):
+#     result = {"Item_id": item_id, **item.dict()}
+#     if q:
+#         result.update({"q" : q})
+#     return result
+
+
+# Qurey Parameters and String Validations
+
+# from typing import Union
+# from fastapi import FastAPI, Query
+# from typing_extensions import Annotated
+
+# app = FastAPI()
+
+# @app.get("/items/")
+# async def read_items(q:Annotated[Union[str, None], Query(max_length=50)] = None):
+#     results = {"ites": [{"item_id": "Foo"}, {"item_id":"Bar"}]}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+# Request Body
+
+# from typing import Union
+# from fastapi import FastAPI
+# from pydantic import BaseModel
+
+# class Item(BaseModel):
+#     name: str
+#     description: Union[str, None] = None
+#     price: float
+#     tax: Union[float, None] = None
+
+# app = FastAPI()
+
+# @app.post("/items/")
+# async def create_item(item: Item):
+#     item_dict = item.dict()
+#     if item.tax:
+#         priceWithTax = item.price + item.tax
+#         item_dict.update ({"Price with tax" : priceWithTax})
+#     print(item_dict)
+#     return item_dict
+
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item):
+#     return {"item_id":item_id, **item.dict()}
+
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item, q: Union[str, None] = None):
+#     result = {"Item_id": item_id, **item.dict()}
+#     if q:
+#         result.update({"q": q})
+#     return result
+
+
+# Query Parameter and String Validations
+
+# from typing import Union
+
+# from fastapi import FastAPI, Query
+# from typing_extensions import Annotated
+
+# app = FastAPI()
+
+# @app.get("/items")
+# async def read_items(
+#    int, 
+    #     Path(title="The ID of the item to get")
+    # ],
+    # q:Annotated[
+    #     Union[str, None],
+    #     Query(alias="item-query")
+    # ]= None    q: Annotated[
+#         Union[str, None],
+#         Query(
+#             alias= "item-query",
+#             title= "Query String",
+#             description= "Query string for the items to search in the database",
+#             min_length=3,
+#             max_length=50,
+#             pattern="^fixedquery$",
+#             deprecated=True
+#         )
+#     ] = None
+# ):
+#     results = {"items": [{"item_id": "Foo"},{"item_id": "Bar"}]}
+#     if q:
+#         results.update({"q":q})
+#     return results
+
+
+# #Path Parameters and Numeric Validations
+# from fastapi import FastAPI, Path, Query
+# from typing_extensions import Annotated
+
+# app =  FastAPI()
+# @app.get("/items/{item_id}")
+# async def read_items(
+#     *,
+#     item_id: Annotated[int, Path(title="The Id of the item to get", ge=0, le=1000)],
+#     q: str, 
+#     size: Annotated[float, Query(gt=0, lt=10.5)]
+# ):
+#     results = {"item_id": item_id}
+#     if q:
+#         results.update({"q": q})
+#     if size:
+#         results.update({"size": size})
+#     return results
+
+## Qurey patameters with a Pydantic Model
+
+# from typing import List
+
+# from fastapi import FastAPI, Query
+# from pydantic import BaseModel, Field
+# from typing_extensions import Annotated, Literal
+
+# app = FastAPI()
+
+# class FilterParams(BaseModel):
+#     model_config = {"extra": "forbid"}
+
+#     limit: int = Field(100, gt=0, le=100)
+#     offset: int = Field(0, ge=0)
+#     order_by: Literal["created_at", "updated_at"] = "created_at",
+#     tags: List[str] = []
+
+# @app.get("items/")
+# async def read_items(filter_query: Annotated[FilterParams, Query()]):
+#     return filter_query
+
+
+## Body - Multiple Parameters
+# from typing import Union
+
+# from fastapi import FastAPI, Path, Body
+# from pydantic import BaseModel
+# from typing_extensions import Annotated
+
+# app = FastAPI()
+
+# class Item(BaseModel):
+#     name: str
+#     description: Union[str, None] = None
+#     price: float
+#     tax: Union[float, None] = None
+
+# class User(BaseModel):
+#     username: str
+#     full_name: Union[str, None] = None
+
+# @app.put("/items/{item_id}")
+# async def update_item(
+#     *,
+#     item_id: int, 
+#     item: Item,
+#     user: User, 
+#     importance: Annotated[int, Body()],
+#     q: Union[str, None] = None
+# ):
+#     results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+
+#Body - Fields
+
+# from typing import Union
+# from fastapi import Body, FastAPI
+# from pydantic import BaseModel, Field
+# from typing_extensions import Annotated
+
+# app = FastAPI()
+
+# class Item(BaseModel):
+#     name: str
+#     description: Union[str, None] = Field(default=None, title="The description of the item", max_length=300)
+#     price: float = Field(gt=0, description="The price must be greater than zero")
+#     tax: Union[float, None] = None
+
+# @app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
+#     results = {"item_id": item_id}
+#     return results
+
+
+#Body - Nested Models
+
+# from typing import List, Set, Union
+
+# from fastapi import FastAPI
+# from pydantic import BaseModel, HttpUrl
+
+# app = FastAPI()
+
+# class Image(BaseModel):
+#     url: HttpUrl
+#     name: str
+
+# class Item(BaseModel):
+#     name: str
+#     description: Union[str, None] = None
+#     price: float
+#     tax: Union[float, None] = None
+#     tags: Set[str] = set()
+#     images: Union[List[Image], None] = None
+
+# class Offer(BaseModel):
+#     name: str
+#     description: Union[str, None] = None
+#     price: float
+#     items: List[Item]
+
+# @app.post("/offers/")
+# async def create_offer(offer: Offer):
+#     return offer
+
+# @app.post("/images/multiple/")
+# async def create_multiple_images(images: List[Image]):
+#     for image in images:
+#         urlid = image.url
+#         urlName = image.name
+#     return images
+
+## Declare Request Example Data
+
+# from typing import Union
+# from fastapi import FastAPI, Body
+# from pydantic import BaseModel
+# from typing_extensions import Annotated
+
+# app = FastAPI()
+
+# class Item(BaseModel):
+#     name: str
+#     description: Union[str, None] = None
+#     price: float
+#     tax: Union[float, None] = None
+
+# @app.put("/items/{item_id}")
+# async def update_item(
+#     *,
+#     item_id: int,
+#     item: Annotated[
+#     Item,
+#     Body(
+#        openapi_examples={
+#            "normal":{
+#                "summary": "A normal example",
+#                "description": "A **normal** item works correctly",
+#                "value":{
+#                    "name": "Foo",
+#                    "description": "A very nice Item",
+#                    "price": 35.4,
+#                    "tax": 3.2
+#                },
+#            },
+#            "converted":{
+#                "summary": "An example with converted Data",
+#                "description": "FastAPI can convert price string",
+#                "value":{
+#                    "name": "Bar",
+#                    "price": "35.4",
+#                }
+#            },
+#            "invalid":{
+#                "summary": "Invalid data is rejected with an error",
+               
+
+#                "value":{
+                   
+#                    "name": "Baz",
+#                    "price": "thirty five point four"
+#                }
+#            }
+#        }
+#     )
+#     ]
+# ):
+#     results = {"item_id": item_id, "item": item}
+#     return results
+
+# Extra Data Types
+# from datetime import datetime, time, timedelta
+# from typing import Union
+# from uuid import UUID
+
+# from fastapi import Body, FastAPI
+# from typing_extensions import Annotated
+
+# app = FastAPI()
+
+# @app.put("/items/{item_id}")
+# async def read_items(
+#     item_id: UUID,
+#     start_datetime: Annotated[datetime, Body()],
+#     end_datetime: Annotated[datetime, Body()],
+#     process_after: Annotated[timedelta, Body()],
+#     repeat_at: Annotated[Union[time, None], Body()]= None,
+# ):
+#     start_process = start_datetime + process_after
+#     duration = end_datetime - start_process
+#     return {
+#         "item_id": item_id,
+#         "start_datetime": start_datetime,
+#         "end_datetime": end_datetime,
+#         "process_after": process_after,
+#         "repeat_at": repeat_at,
+#         "start_process": start_process,
+#         "duration": duration
+#     }
+
+
+# "Cookie Parameters
+# from typing import Union
+
+# from fastapi import FastAPI, Cookie
+# from typing_extensions import Annotated
+
+# app = FastAPI()
+
+# @app.get("/items")
+# async def read_items(ads_id: Annotated[Union[str, None], Cookie()]= None):
+#     return {"ads_id": ads_id}
+
+
+#Header Parameters
+
+# from typing import Union, List
+
+# from fastapi import FastAPI, Header
+# from typing_extensions import Annotated
+
+# app = FastAPI()
+
+# @app.get("/items/")
+# async def read_iems(
+#     x_token: Annotated[
+#         Union[List[str], None], Header()
+#     ] = None
+# ):
+#     return {"X-Token Values":x_token}
+
+# Cookie Parameter Model
+
+# from typing import Union
+
+# from fastapi import FastAPI, Cookie
+# from pydantic import BaseModel
+# from typing_extensions import Annotated
+
+# app = FastAPI()
+
+# class Cookies(BaseModel):
+#     session_id: str
+#     fatebook_traker: Union[str, None] = None
+#     googall_tracker: Union[str, None] = None
+
+# @app.get("/items/")
+# async def read_items(cookies: Annotated[Cookies, Cookie()]):
+#     return cookies
+
+
+#Header Parameter Models
+
+# from typing import List, Union
+
+# from fastapi import FastAPI, Header
+# from pydantic import BaseModel
+# from typing_extensions import Annotated
+
+# app = FastAPI()
+
+# class CommonHeaders(BaseModel):
+#     host: str
+#     save_data: bool
+#     if_modified_since: Union[str, None] = None
+#     traceparent: Union[str, None] = None
+#     x_tag: List[str] = []
+
+# @app.get("/items/")
+# async def read_items(headers: Annotated[CommonHeaders, Header()]):
+#     return headers
+
+# Response Model - Return Type
+
+from typing import List, Union, Any
+
+from fastapi import FastAPI , Response
+from fastapi.responses import JSONResponse, RedirectResponse
+from pydantic import BaseModel, EmailStr
+
+app = FastAPI()
 
 class Item(BaseModel):
     name: str
     description: Union[str, None] = None
     price: float
-    tax: Union[float, None] = None
+    tax: float = 10.5
+    tags: List[str] = []
 
-app = FastAPI()
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {"name": "Bar", "description": "The bartenders", "price":62, "tax": 20.2 },
+    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []}
+}
 
-@app.post("/items/")
-async def create_item(item: Item):
-    item_dict = item.dict()
-    if item.tax:
-        price_with_tax = item.price + item.tax
-        item_dict.update({"Price_with_tax" : price_with_tax})
-    return item_dict
+@app.get("/items/{item_id}", response_model=Item, response_model_include= {'name', 'description','price'})
+async def read_item_name(item_id: str):
+    return items[item_id]
 
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item, q: Union[str, None] = None):
-    result = {"Item_id": item_id, **item.dict()}
-    if q:
-        result.update({"q" : q})
-    return result
+@app.get("/items/{item_id}/public", response_model= Item, response_model_exclude={'tax'})
+async def read_item_public_data(item_id: str):
+    return items[item_id]
